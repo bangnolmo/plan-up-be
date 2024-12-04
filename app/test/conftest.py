0ecb9c1873ef8d26de5e0925f2c55649c664934b
heung115 <heung115@naver.com>
2024-12-04 18:25:36 +0900

    Feat: update GitHub Actions workflow to include MySQL service and wait for readiness; modify pytest configuration for environment variables

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -2,17 +2,17 @@ import pytest
 from unittest.mock import patch, MagicMock
 
 
-@pytest.fixture(autouse=True)
-def mock_db_connection():
-    with patch("app.utils.db_driver.get_conn_and_cursor") as mock_conn:
-        mock_cursor = MagicMock()
-        mock_conn.return_value = (MagicMock(), mock_cursor)
-        print("Mocked get_conn_and_cursor")
-        yield
+# @pytest.fixture(autouse=True)
+# def mock_db_connection():
+#     with patch("app.utils.db_driver.get_conn_and_cursor") as mock_conn:
+#         mock_cursor = MagicMock()
+#         mock_conn.return_value = (MagicMock(), mock_cursor)
+#         print("Mocked get_conn_and_cursor")
+#         yield
 
 
-@pytest.fixture(autouse=True)
-def mock_mysql_connector():
-    with patch("mysql.connector.connect", return_value=MagicMock()) as mock_connect:
-        print("Mocked mysql.connector.connect")
-        yield
+# @pytest.fixture(autouse=True)
+# def mock_mysql_connector():
+#     with patch("mysql.connector.connect", return_value=MagicMock()) as mock_connect:
+#         print("Mocked mysql.connector.connect")
+#         yield
